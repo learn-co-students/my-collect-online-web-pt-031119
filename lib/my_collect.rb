11ad468892f445e1b985d9f6582096d3630223e0
Kat B <katreesebarnes@gmail.com>
2019-03-18 21:31:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+require "pry"
+def my_collect(array)
+  x = 0 
+  capital_elements = []
+  while x < array.length
+  
+ capital_elements << yield(array[x])
+  
+  x += 1
+  
 
+ end
+  capital_elements
+end
 
